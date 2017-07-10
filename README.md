fa1f0ba9a3b15ba24dc3be9e37b61123ee25f6a5
Luis Vega <lvegat@gmail.com>
2017-07-09 22:28:34 -0600

    SigleTon exammpled added

--- README.md
+++ README.md
@@ -6,3 +6,6 @@ Abstrac Factory
 	Provide an interface for creating families of related or dependent objects without specifying their concrete classes.
 Abstract Method
  	Define an interface for creating an object, but let subclasses decide which class to instantiate. Factory Method lets a class defer instantiation to subclasses
+
+Singleon
+	Ensure a class has only one instance and provide a global point of access to it.
---
+++ singleon/Program.cs
@@ -0,0 +1,91 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace singleon
+{
+    /// <summary>
+    /// Ensure a class has only one instance and provide a global point of access to it.
+    /// </summary>
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            LoadBalancer b1 = LoadBalancer.GetLoadBalancer();
+            LoadBalancer b2 = LoadBalancer.GetLoadBalancer();
+            LoadBalancer b3 = LoadBalancer.GetLoadBalancer();
+            LoadBalancer b4 = LoadBalancer.GetLoadBalancer();
+
+            // Same instance?
+            if (b1 == b2 && b2 == b3 && b3 == b4)
+            {
+                Console.WriteLine("Same instance\n");
+            }
+
+            LoadBalancer balancer = LoadBalancer.GetLoadBalancer();
+            for (int i = 0; i < 15; i++)
+            {
+                string server = balancer.Server;
+                Console.WriteLine("Dispatch Request to: " + server);
+            }
+
+            // Wait for user
+            Console.ReadKey();
+        }
+    }
+
+    class LoadBalancer
+    {
+        private static LoadBalancer _instance;
+        private List<string> _servers = new List<string>();
+        private Random _radom = new Random();
+
+        // Lock synchronization object
+        private static object syncLock = new object();
+
+        // Constructor (protected)
+        protected LoadBalancer()
+        {
+            // List of available servers
+            _servers.Add("ServerI");
+            _servers.Add("ServerII");
+            _servers.Add("ServerIII");
+            _servers.Add("ServerIV");
+            _servers.Add("ServerV");
+        }
+
+        public static LoadBalancer GetLoadBalancer()
+        {
+            // Support multithreaded applications through
+            // 'Double checked locking' pattern which (once
+            // the instance exists) avoids locking each
+            // time the method is invoked
+            if (_instance == null)
+            {
+                lock (syncLock)
+                {
+                    if (_instance == null)
+                    {
+                        _instance = new LoadBalancer();
+                    }
+                }
+            }
+
+            return _instance;
+        }
+
+
+        // Simple, but effective random load balancer
+        public string Server
+        {
+            get
+            {
+                int r = _radom.Next(_servers.Count);
+                return _servers[r].ToString();
+            }
+        }
+
+    }
+}
